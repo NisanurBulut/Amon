1d4c21fcd275bcc918416c4d169a79efb11e1a94
NisanurBulut <nbulut@sarplojistik.com>
2021-03-28 22:58:46 +0300

    implemented demand list

--- resources/views/demands/index.blade.php
+++ resources/views/demands/index.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="ui middle large aligned center aligned grid" id="content">
-        <div class="column">
-            <div class="ui cards">
-                @for ($i=0; $i<20; $i++)
-                <x-demand-item />
+    <div class="ui middle large center aligned aligned grid" id="content">
+        <div class="html center ui top attached segment">
+            <div class="ui huge top attached label">
+                <i style="float: right;" data-content="Copy code" class="plus icon"></i>
+            </div>
+            <div class="ui cards" style="padding-top: 15px;">
+                @for ($i = 0; $i < 20; $i++)
+                    <x-demand-item />
                 @endfor
             </div>
         </div>
