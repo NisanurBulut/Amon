1770f084b71923d5fab813384f1a72fd9c976c3c
NisanurBulut <nbulut@sarplojistik.com>
2021-03-28 23:21:31 +0300

    created confirm component

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $('.tabItem').tab();
+    $('.ui.modal')
+  .modal('show')
+;
 });
---
+++ resources/views/components/shared/confirm.blade.php
@@ -0,0 +1,4 @@
+<div class="ui small modal transition visible active">
+    <div class="content">Are you sure?</div>
+    <div class="actions"><button class="ui button">Cancel</button><button class="ui primary button">OK</button></div>
+</div>
