1bc73de6307ee148cd6f806da9f64f8f9c2365c7
NisanurBulut <nbulut@sarplojistik.com>
2021-03-28 22:14:47 +0300

    implemented demand state status component views

---
+++ resources/views/components/demand-state.blade.php
@@ -0,0 +1,43 @@
+<table id="dtDemandStatus" class="ui celled table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>İsim</th>
+            <th>İşlem</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td data-label="Id">1</td>
+            <td data-label="İsim">Görüldü</td>
+            <td>
+                <a><i class="blue edit icon"></i></a>
+                <a><i class="red trash icon"></i></a>
+            </td>
+        </tr>
+        <tr>
+            <td data-label="Id">1</td>
+            <td data-label="İsim">Devam ediyor</td>
+            <td>
+                <a><i class="blue edit icon"></i></a>
+                <a><i class="red trash icon"></i></a>
+            </td>
+        </tr>
+        <tr>
+            <td data-label="Id">1</td>
+            <td data-label="İsim">Tamamlandı</td>
+            <td>
+                <a><i class="blue edit icon"></i></a>
+                <a><i class="red trash icon"></i></a>
+            </td>
+        </tr>
+        <tr>
+            <td data-label="Id">1</td>
+            <td data-label="İsim">Onaylandı</td>
+            <td>
+                <a><i class="blue edit icon"></i></a>
+                <a><i class="red trash icon"></i></a>
+            </td>
+        </tr>
+    </tbody>
+</table>
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -8,90 +8,10 @@
         </div>
         <div class="main ui container">
             <div class="ui active tab segment" data-tab="tabDemandStatus" style="">
-                <table id="dtDemandStatus" class="ui celled table">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>İsim</th>
-                            <th>Renk</th>
-                            <th>işlem</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Normal</td>
-                            <td>blue</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="Name">2</td>
-                            <td data-label="Age">Acil</td>
-                            <td data-label="Age">orange</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="Name">3</td>
-                            <td data-label="Age">Çok Acil</td>
-                            <td data-label="Age">red</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <x-demand-status />
             </div>
             <div class="ui tab segment" data-tab="tabDemandState" style="">
-                <table id="dtDemandStatus" class="ui celled table">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>İsim</th>
-                            <th>İşlem</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td data-label="Id">1</td>
-                            <td data-label="İsim">Görüldü</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="Id">1</td>
-                            <td data-label="İsim">Devam ediyor</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="Id">1</td>
-                            <td data-label="İsim">Tamamlandı</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="Id">1</td>
-                            <td data-label="İsim">Onaylandı</td>
-                            <td>
-                                <a><i class="blue edit icon"></i></a>
-                                <a><i class="red trash icon"></i></a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <x-demand-state />
             </div>
         </div>
     @endsection
