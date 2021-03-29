8504e007d8d5533ada087c594f8b565d2dd05c57
NisanurBulut <nbulut@sarplojistik.com>
2021-03-29 10:08:10 +0300

    defined state color component

---
+++ resources/views/components/state-color.blade.php
@@ -0,0 +1,18 @@
+<x-shared.modal />
+<div class="ui top attached segment">
+<a id="btnStateAdd" class="ui huge top attached label btnModalOpen" href="/settings/createColor">
+    <i style="float: right;" data-content="Copy code" class="plus icon"></i>
+</a>
+<table id="dtColor" class="ui celled table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>İsim</th>
+            <th>Açıklama</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+</div>
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -6,6 +6,7 @@
             <div class="ui four item stackable tabs menu" id="settingsTab">
                 <a class="tabItem item active" data-tab="tabDemandStatus">Talep Durumları</a>
                 <a class="tabItem item" data-tab="tabDemandState">Talep Aşamaları</a>
+                <a class="tabItem item" data-tab="tabStatusColor">Renk Tanımları</a>
             </div>
             <div class="main ui container">
                 <div class="ui active tab segment" data-tab="tabDemandStatus" style="">
@@ -13,6 +14,9 @@
                 </div>
                 <div class="ui tab segment" data-tab="tabDemandState" style="">
                     <x-demand-state />
+                </div>
+                <div class="ui tab segment" data-tab="tabStatusColor" style="">
+                    <x-state-color />
                 </div>
             </div>
     </div>
