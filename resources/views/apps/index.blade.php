8bfbe6175dfeaf308935f310fb995f1f4dbc2bd7
NisanurBulut <nbulut@sarplojistik.com>
2021-03-30 13:14:40 +0300

    listed mockup datas for app index

---
+++ resources/views/apps/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+@section('content')
+<div class="html ui top attached segment" id="content">
+    <div class="ui grid">
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+        <div class="four wide column"><img class="ui image" src="https://picsum.photos/200/300" /></div>
+
+      </div>
+  <div class="ui top attached label">Example <i data-content="Copy code" class="copy link icon"></i></div></div>
+@endsection
