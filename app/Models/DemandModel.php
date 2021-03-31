0da2cd295835f37f2b6dd7aab88ea4a0deb59d46
NisanurBulut <nbulut@sarplojistik.com>
2021-03-31 13:50:53 +0300

    migrated tdemand table

--- app/Models/DemandModel.php
+++ app/Models/DemandModel.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class DemandModel extends Model
 {
     use HasFactory;
+    protected $table = "tdemand";
+    protected $fillable=["title",
+                         "description",
+                         "state_id",
+                         "status_id",
+                         "app_id",
+                         "owner_id",
+                         "undertaking_id"];
 }
--- database/migrations/2021_03_31_104103_create_table_tdemand.php
+++ database/migrations/2021_03_31_104103_create_table_tdemand.php
@@ -13,8 +13,15 @@ class CreateTableTdemand extends Migration
      */
     public function up()
     {
-        Schema::create('table_tdemand', function (Blueprint $table) {
+        Schema::create('tdemand', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('state_id')->nullable();
+            $table->integer('status_id');
+            $table->integer('owner_id');
+            $table->integer('undertaking_id');
+            $table->integer('app_id');
             $table->timestamps();
         });
     }
@@ -26,6 +33,6 @@ class CreateTableTdemand extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_tdemand');
+        Schema::dropIfExists('tdemand');
     }
 }
