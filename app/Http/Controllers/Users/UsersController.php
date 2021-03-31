40ca4acea0f1b9a2b3714c54f68fa1d3d310b86f
NisanurBulut <nbulut@sarplojistik.com>
2021-03-31 10:39:29 +0300

    implemented store user

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -18,4 +18,31 @@ class UsersController extends Controller
     {
         return View('users.forms.create-user');
     }
+    function storeUser(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'is_admin'=> 'required',
+            'username'=>'required',
+            'image_url'=>'required|max:200'
+        ]);
+
+        $user = new User();
+        $user->name= $request['name'];
+        $user->username=$request['username'];
+        $user->email= $request['email'];
+        $user->password=$request['password'];
+        if($request['is_admin']=="on")
+        {
+            $user->is_admin = true;
+        }else{
+            $user->is_admin = false;
+        }
+        $user->image_url=$request['image_url'];
+
+        $user->save();
+        return redirect('users.index');
+    }
 }
--- resources/views/users/forms/create-user.blade.php
+++ resources/views/users/forms/create-user.blade.php
@@ -1,33 +1,40 @@
 <form autoComplete="off" class="ui form" method="POST" action="{{ route('users.storeUser') }}">
     @csrf
     <h4 class="ui dividing header">Kullanıcı Bilgileri</h4>
-    <div class="two fields">
-
+    <div class="three fields">
         <div class="field">
             <label>Ad Soyad</label>
             <div class="ui right labeled input">
-                <input id="name" type="text" name="name" placeholder="Uygulama ikon adresi"
+                <input id="name" type="text" name="name" placeholder="Kullanıcı adı ve soyadı"
                  required
-                onkeyup="event.preventDefault();countInput(this,'lblUser_name')" maxlength="300"/>
-                <div class="ui basic label label" id="lblUser_name">0</div>
+                onkeyup="event.preventDefault();countInput(this,'lblUser_name')" maxlength="255"/>
+                <div class="ui basic label label" id="lblUser_name">255</div>
             </div>
         </div>
         <div class="field">
             <label>Kullanıcı Adı</label>
             <div class="ui right labeled input">
-                <input name="username" required placeholder="Kullanıcı Adı"
+                <input name="username" required placeholder="Kullanıcı adı" maxlength="255"
                 onkeyup="event.preventDefault();countInput(this,'lblUsername')"/>
-                <div class="ui basic label label" id="lblUsername">0</div>
+                <div class="ui basic label label" id="lblUsername">255</div>
+            </div>
+        </div>
+        <div class="field">
+            <label>Parola</label>
+            <div class="ui right labeled input">
+                <input name="password" required placeholder="Parola" maxlength="6"
+                onkeyup="event.preventDefault();countInput(this,'lblPassword')"/>
+                <div class="ui basic label label" id="lblPassword">6</div>
             </div>
         </div>
     </div>
     <div class="field">
         <label>E-Posta Adresi</label>
         <div class="ui right labeled input">
-        <input name="email" placeholder="E-Posta Adresi"
+        <input type="email" name="email" placeholder="E-Posta adresi" maxlength="255"
         onkeyup="event.preventDefault();countInput(this,'lblUserEmail')"
         />
-        <div class="ui basic label label" id="lblUserEmail">0</div>
+        <div class="ui basic label label" id="lblUserEmail">255</div>
         </div>
     </div>
 
@@ -37,15 +44,19 @@
             <div class="twelve wide field">
                 <div class="ui right labeled input">
                     <input id="image_url" type="text" name="image_url"
-                    placeholder="Kullanıcı Profil Fotoğrafı"
-                     required onchange="$('#image_url').attr('src',this.value)"
+                    placeholder="Kullanıcı profil fotoğrafı"
+                     required
+                     onchange="$('#image_url_src').attr('src',this.value)"
                      onkeyup="event.preventDefault();countInput(this,'lblUserImageUrl')"
                     maxlength="300"/>
-                    <div class="ui basic label label" id="lblUserImageUrl">0</div>
+                    <div class="ui basic label label" id="lblUserImageUrl">300</div>
                 </div>
             </div>
             <div class="four wide field">
-                <img id="image_url" class="ui middle aligned tiny image" src="http://placehold.jp/200x150.png" />
+                <img id="image_url_src"
+                name="image_url"
+                class="ui middle aligned tiny image"
+                src="http://placehold.jp/200x150.png" />
             </div>
         </div>
     </div>
