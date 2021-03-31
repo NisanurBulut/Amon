846fd006b0fcb1946d7c1716d6fa6afc98e9c024
NisanurBulut <nbulut@sarplojistik.com>
2021-03-31 21:12:59 +0300

    according to by admin selected demands list

--- app/Http/Controllers/Demands/DemandsController.php
+++ app/Http/Controllers/Demands/DemandsController.php
@@ -26,22 +26,43 @@ class DemandsController extends Controller
     }
     public function index()
     {
-        $demands = DB::table('tdemand')
-            ->join('tstatus', 'tstatus.id', '=', 'tdemand.status_id')
-            ->join('tcolor', 'tcolor.id', '=', 'tstatus.color_id')
-            ->join('users', 'users.id', '=', 'tdemand.owner_id')
-            ->leftJoin('users as tuser', 'tuser.id', '=', 'tdemand.undertaking_id')
-            ->join('tapp', 'tapp.id', '=', 'tdemand.app_id')
-            ->join('tstate', 'tstate.id', '=', 'tdemand.state_id')
-            ->select('tdemand.id','tdemand.title','tdemand.description','tdemand.created_at',
-            'tapp.name as appName',
-            'users.name as ownerName',
-            'users.image_url as ownerImage',
-            'tuser.name as underName',
-            'tuser.image_url as underImage',
-            'tcolor.name as color',
-            'tstatus.name as status',
-            'tstate.name as state')->orderBy('tdemand.created_at', 'DESC')->get();
+        $user = Auth::user();
+        if($user->is_admin==true)
+
+            $demands = DB::table('tdemand')
+                ->join('tstatus', 'tstatus.id', '=', 'tdemand.status_id')
+                ->join('tcolor', 'tcolor.id', '=', 'tstatus.color_id')
+                ->join('users', 'users.id', '=', 'tdemand.owner_id')
+                ->leftJoin('users as tuser', 'tuser.id', '=', 'tdemand.undertaking_id')
+                ->join('tapp', 'tapp.id', '=', 'tdemand.app_id')
+                ->join('tstate', 'tstate.id', '=', 'tdemand.state_id')
+                ->select('tdemand.id','tdemand.title','tdemand.description','tdemand.created_at',
+                'tapp.name as appName',
+                'users.name as ownerName',
+                'users.image_url as ownerImage',
+                'tuser.name as underName',
+                'tuser.image_url as underImage',
+                'tcolor.name as color',
+                'tstatus.name as status',
+                'tstate.name as state')->orderBy('tdemand.created_at', 'DESC')->get();
+                else
+                $demands = DB::table('tdemand')
+                ->where('owner_id',$user->id)
+                ->join('tstatus', 'tstatus.id', '=', 'tdemand.status_id')
+                ->join('tcolor', 'tcolor.id', '=', 'tstatus.color_id')
+                ->join('users', 'users.id', '=', 'tdemand.owner_id')
+                ->leftJoin('users as tuser', 'tuser.id', '=', 'tdemand.undertaking_id')
+                ->join('tapp', 'tapp.id', '=', 'tdemand.app_id')
+                ->join('tstate', 'tstate.id', '=', 'tdemand.state_id')
+                ->select('tdemand.id','tdemand.title','tdemand.description','tdemand.created_at',
+                'tapp.name as appName',
+                'users.name as ownerName',
+                'users.image_url as ownerImage',
+                'tuser.name as underName',
+                'tuser.image_url as underImage',
+                'tcolor.name as color',
+                'tstatus.name as status',
+                'tstate.name as state')->orderBy('tdemand.created_at', 'DESC')->get();
 
         return View('demands.index', ["demands"=>$demands]);
     }
